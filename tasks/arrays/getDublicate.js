216a156ebb05380abd90f22c63e202c0ab9ec82a
tanaprog <tananaeva.art@gmail.com>
2023-06-30 12:08:34 +0300

    changet functions one more time

--- tasks/arrays/getDublicate.js
+++ tasks/arrays/getDublicate.js
@@ -16,18 +16,50 @@
  * @args arg: array
  */
 
-function getDublicate(array) {
-    let result = {}
-    for (let i = 0; i < array.length; ++i) {
+// function getDublicate(array) {
+//     let result = {}
+//     for (let i = 0; i < array.length; i++) {
+//         let num = array[i]
+//         if (result[num] != undefined)
+//             result[num]++
+//         else
+//             result[num] = 1
+//     }
+//     for (let key in result) {
+//         console.log('число ' + key + ' : ' + result[key])
+//     }
+// }
+// console.log(`${getDublicate([1, 1, 1, 3, 23, 23, -5])}`);
+// console.log(`${getDublicate([1, 1, 1, 1, 1, 1, 1])}`);
+
+function getDublicate(array){
+    let result = []
+    for(let i = 0; i < array.length; i++){
         let num = array[i]
-        if (result[num] != undefined)
-            ++result[num]
-        else
+        if(result[num] != undefined)
+            result[num]++
+        else 
             result[num] = 1
     }
-    for (let key in result) {
-        console.log('число ' + key + ' : ' + result[key])
+        for (let item in result){
+            console.log('число ' + item + ' : ' + result[item])
+        }
     }
+console.log(`${getDublicate([1, 1, 1, 3, 23, 23, -5])}`);
+console.log(`${getDublicate([1, 1, 1, 1, 1, 1, 1])}`);
+
+
+
+function getDublicate(array){
+    const result = {}
+    for(let element of array){
+        if(result[element] != undefined){
+            result[element]++
+        }else{
+            result[element] = 1
+        }
+    }
+    return console.log(result)
 }
 console.log(`${getDublicate([1, 1, 1, 3, 23, 23, -5])}`);
 
--- tasks/arrays/sort.js
+++ tasks/arrays/sort.js
@@ -8,7 +8,7 @@
 function sortArray(array) {
     return array.sort((a, b) => a - b)
 }
-console.log(`Сортировка по Возрастанию ${sortArray([5, 3, 23, 1, 18,])}`);
+console.log(`Сортировка по Возрастанию ${sortArray([5, 3, 23, 1, 18])}`);
 
 
 // Сдеать самой, без стандартного метода массива для сортировки
@@ -16,11 +16,11 @@ console.log(`Сортировка по Возрастанию ${sortArray([5, 3,
 function mySort(array) {
     let arrCopy = array.slice(0, array.length)
     let result = []
-    arrCopy.forEach(() => {
+    arrCopy.forEach(() => { //forEach - перебор элементов массива
         let minElementIndex = 0
         minElementIndex = array.indexOf(Math.min.apply(Math, array))//indexOf() возвращает первый индекс
         result.push(array[minElementIndex])//с помощью push() добавляются в другой массив
-        array.splice(minElementIndex, 1)
+        array.splice(minElementIndex, 1) //удаляет один эл. по индексу minElementIndex
     })
     return result
 }
